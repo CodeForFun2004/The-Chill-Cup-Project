3a040b8490d2180f16a931aeb0cebcfca3d4b727
Quoc Huy <huydqds180257@fpt.edu.vn>
2025-07-22 12:32:35 +0700

    Done UI Chatbot

--- frontend/api/axios.ts
+++ frontend/api/axios.ts
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 //const API_URL = 'http://192.168.110.7:8080/api';
 
-const API_URL = 'http://192.168.110.23:8080/api';
+const API_URL = 'http://10.12.49.254:8080/api';
 
 
 let apiInstance = axios.create({
